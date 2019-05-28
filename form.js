af3dc806a9433d53327968fe6f732e198a325ae7
bricemodeste <bricemodeste@Brices-MacBook-Pro.local>
2019-05-28 09:40:37 +0200

    help from jonas to check email name on databases , thank you our savior

--- form.js
+++ form.js
@@ -1,5 +1,5 @@
 const data = {};
-
+const form = document.querySelector("form");
 ("use strict");
 
 function post(newUsers) {
@@ -14,11 +14,26 @@ function post(newUsers) {
   })
     .then(res => res.json())
     .then(data => {
-      form.elements.submit.disabled = false;
-      showUsers(data);
+      console.log(data);
+      if (data.message) {
+        //unhappy
+        const generalError = data.message;
+        const field = data.list[0].field;
+        const specificError = data.list[0].message[0];
+        console.log(generalError, specificError, field);
+        console.log("the " + field + specificError);
+        //TODO: show error
+      } else {
+        //happy
+        form.style.display = "none";
+        document.querySelector(".main-essai").style.display = "block";
+        document.querySelector(".main-essai span").textContent = newUsers.name;
+      }
+      /*form.elements.submit.disabled = false;
+      showUsers(data);*/
     });
 }
-
+/*
 function get() {
   fetch("https://testdb-6ac0.restdb.io/rest/oddset-users", {
     method: "get",
@@ -34,7 +49,8 @@ function get() {
       data.forEach(showUsers);
     });
 }
-
+*/
+/*
 function showUsers(oddsetUsers) {
   const template = document.querySelector("template").content;
   const clone = template.cloneNode(true);
@@ -45,8 +61,8 @@ function showUsers(oddsetUsers) {
 }
 
 get();
+*/
 
-const form = document.querySelector("form");
 form.addEventListener("submit", e => {
   form.elements.submit.disabled = true;
   e.preventDefault();
